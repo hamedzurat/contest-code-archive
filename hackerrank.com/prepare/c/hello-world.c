c61e69297b74b672dacbf5883f0cfb8e24e7bb3e
hamed zurat bin hashem <38398613+hamedzurat@users.noreply.github.com>
2024-03-03 20:46:49 +0600

    backup from (Arch Linux) on "2024-03-03--20:46"

---
+++ hackerrank.com/prepare/c/hello-world.c
@@ -0,0 +1,24 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+
+    char s[100];
+    scanf("%[^\n]%*c", &s);
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+
+    printf("Hello, World!\n");
+    printf("%s", s);
+
+    // for(int i=0; i< 100; i++){
+    //     if(s[i]=='\0')
+    //         break;
+
+    //     printf("%c", s[i]);
+    // }
+
+    return 0;
+}
