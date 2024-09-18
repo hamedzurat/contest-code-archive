0d6ed2522b5557d20d57d1c2c2c5d918a8ff2e4a
hamed zurat bin hashem <38398613+hamedzurat@users.noreply.github.com>
2024-09-18 19:02:11 +0600

    backup from (Arch Linux) on "2024-09-18--19:02"

---
+++ exercism.org/java/logs-logs-logs/src/main/java/LogLine.java
@@ -0,0 +1,26 @@
+public class LogLine {
+    private final LogLevel l;
+    private final String message;
+
+    public LogLine(String logLine) {
+        l = switch (logLine.substring(1, 4)) {
+            case "TRC" -> LogLevel.TRACE;
+            case "DBG" -> LogLevel.DEBUG;
+            case "INF" -> LogLevel.INFO;
+            case "WRN" -> LogLevel.WARNING;
+            case "ERR" -> LogLevel.ERROR;
+            case "FTL" -> LogLevel.FATAL;
+            default -> LogLevel.UNKNOWN;
+        };
+
+        message = logLine.split("]: ", 100)[1].trim();
+    }
+
+    public LogLevel getLogLevel() {
+        return l;
+    }
+
+    public String getOutputForShortLog() {
+        return l.getCode() + ":" + message;
+    }
+}
