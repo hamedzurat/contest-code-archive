c4ced542b3a91a593a33b08d3070127cda4af3ee
hamed zurat bin hashem <38398613+hamedzurat@users.noreply.github.com>
2024-09-20 17:26:10 +0600

    backup from (Arch Linux) on "2024-09-20--17:26"

---
+++ exercism.org/java/darts/src/main/java/Darts.java
@@ -0,0 +1,10 @@
+class Darts {
+    int score(double xOfDart, double yOfDart) {
+        double x = Math.hypot(xOfDart, yOfDart); // Math.sqrt(Math.pow(xOfDart, 2) + Math.pow(yOfDart, 2));
+
+        if (x <= 1) return 10;
+        else if (x <= 5) return 5;
+        else if (x <= 10) return 1;
+        else return 0;
+    }
+}
