c3b2e795aee6d86d483b04cd28d0b068013f2084
hamed zurat bin hashem <38398613+hamedzurat@users.noreply.github.com>
2024-09-20 17:06:42 +0600

    backup from (Arch Linux) on "2024-09-20--17:06"

---
+++ exercism.org/java/armstrong-numbers/src/main/java/ArmstrongNumbers.java
@@ -0,0 +1,18 @@
+class ArmstrongNumbers {
+    boolean isArmstrongNumber(int n) {
+        return n == String.valueOf(n)
+                .chars().parallel()
+                .map(d -> (int) Math.pow(d - '0', String.valueOf(n).length()))
+                .sum();
+
+//        int n = numberToCheck, sum = 0, m = String.valueOf(n).length();
+//
+//        while (n != 0) {
+//            int i = n % 10;
+//            sum += (int) Math.pow(i, m);
+//            n /= 10;
+//        }
+//
+//        return numberToCheck == sum;
+    }
+}
